926c02e55d26db1edb9fe04c3480d569010d235b
Joshua <joshua.fan12@gmail.com>
2022-02-10 02:02:16 -0800

    moved pack.mcdata

--- GenerateDatapack.py
+++ GenerateDatapack.py
@@ -1,12 +1,14 @@
 from GenerateFolders import generateFolders
 from GenerateFiles import generateFiles
 from AddictionHelperGenerator import addictionHelperGenerator
+import shutil
+from Constants import *
 
 generateFolders()
 generateFiles()
 addictionHelperGenerator()
 
-
+shutil.make_archive('tobacco_awareness', 'zip', rootdir)
 
 
 
--- tobacco_awareness/data/tobacco_awareness/functions/addiction/addiction_helper/c.mcfunction
+++ tobacco_awareness/data/tobacco_awareness/functions/addiction/addiction_helper/c.mcfunction
@@ -1,3 +1,3 @@
 # Change addiction score
-execute as @s run scoreboard players add @s Addiction 1
-execute as @s run say "Addiction score increased by 1"
+execute as @s run scoreboard players add @s Addiction 4
+execute as @s run say "Addiction score increased by 4"
